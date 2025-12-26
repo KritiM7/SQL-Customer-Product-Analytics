7f7db1e709674d1e0b1cf4b3369f2cfbee94dd59
KritiM7 <112483899+KritiM7@users.noreply.github.com>
2025-12-26 14:25:17 +0530

    Delete Data import check.sql

--- Data import check.sql
+++
@@ -1,8 +0,0 @@
-USE datawarehouseanalytics;
-SELECT 'customers' as table_name, COUNT(*) FROM gold_dim_customers
-UNION
--- Check Product count
-SELECT 'products', COUNT(*) FROM gold_dim_products
-UNION
--- Check Sales count
-SELECT 'sales', COUNT(*) FROM gold_fact_sales;
